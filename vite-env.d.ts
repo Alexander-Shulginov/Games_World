9f7a10b01aa7296162a05506078ddb4d59cb189c
Alexander Shulginov <Alexander3post@gmail.com>
2026-04-13 11:31:30 +0300

    upd: npm packages

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module 'virtual:svg-icons-register'
+declare module 'swiper/css'
+declare module 'swiper/css/navigation'
+declare module 'swiper/css/pagination'
